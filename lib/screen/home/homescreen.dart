77fdd38097e5350b1f5eb976594d474e5ac6aa27
Jose Ramon Blanco <jr.cosio1@gmail.com>
2026-02-11 22:35:39 +0100

    feat: Implementa la pantalla de inicio con un botón de inicio de sesión de Google y añade el icono de Google.

--- lib/screen/home/homescreen.dart
+++ lib/screen/home/homescreen.dart
@@ -12,11 +12,47 @@ class HomeScreen extends StatelessWidget {
           children: [
             Image.asset(
               'assets/logos/logoutils_v2.webp',
-              width: 300,
-              height: 300,
+              width: 250,
+              height: 250,
               fit: BoxFit.contain,
             ),
-            Text('Útil', style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold, fontFamily: 'Roboto', color: const Color.fromARGB(255, 9, 166, 214))),
+            Text(
+              'Útil',
+              style: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+                color: const Color.fromARGB(255, 9, 166, 214),
+              ),
+            ),
+            const SizedBox(height: 40),
+            FilledButton(
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                // backgroundColor: const Color.fromARGB(255, 38, 168, 207),
+                fixedSize: const Size(240, 80),
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/iconos/google.webp',
+                    width: 48,
+                    height: 48,
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'ENTRAR',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                      // color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
