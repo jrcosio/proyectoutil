538ef5c135b6a75e7d7e76a709fa1f8ff9e53003
Jose Ramon Blanco <jr.cosio1@gmail.com>
2026-02-17 23:08:44 +0100

    feat: Configuración inicial de Firebase con Core, Analytics y Crashlytics, incluyendo dependencias, archivos de opciones y configuración de Android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:proyectoutil/firebase_options.dart';
 import 'package:proyectoutil/screen/login/loginscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //Nos aseguramos de que Flutter se inicialice correctamente
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // FirebaseCrashlytics.instance.crash();
+
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  await analytics.logEvent(
+    name: 'App_Iniciada',
+    parameters: {'timestamp': DateTime.now().toIso8601String()},
+  );
+
   runApp(const MyApp());
 }
 
@@ -10,9 +27,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Proyecto Util',
-      home: const LoginScreen(),
-    );
+    return MaterialApp(title: 'Proyecto Util', home: const LoginScreen());
   }
 }
