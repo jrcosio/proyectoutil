3b2f4a1c67b5eed23cb0e0d3cdade7da6d2ab2e4
Jose Ramon Blanco <jr.cosio1@gmail.com>
2026-02-17 23:10:30 +0100

    feat: Inicializa Firebase con Crashlytics y Analytics, y configura la estructura principal de la aplicación.

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //Nos aseguramos de que Flutter se inicialice correctamente
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError; //Nos aseguramos de que Flutter se inicialice correctamente
 
-  // FirebaseCrashlytics.instance.crash();
+  // FirebaseCrashlytics.instance.crash(); //Crash para probar la funcionalidad
 
-  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance; //Inicializamos FirebaseAnalytics
 
-  await analytics.logEvent(
+  await analytics.logEvent( //Registramos el evento de inicio de la app
     name: 'App_Iniciada',
     parameters: {'timestamp': DateTime.now().toIso8601String()},
   );
